d5bd20d2d51b76436cb1dc2d18318ab956cf96a7
Arnav Kumar <arnav@arnavkumar.com>
2015-12-20 18:55:17 +0800

    Update build and initializer
    - Also reorganize the project for future builds

--- twitter_cldr.js
+++ twitter_cldr.js
@@ -1,6 +1,9 @@
 // Copyright 2012 Twitter, Inc
 // http://www.apache.org/licenses/LICENSE-2.0
 
-exports.load = function(locale) {
-  return require('./full/' + locale);
-}
+exports.load = function (locale) {
+  var TwitterCLDR = require('./assets/twitter_cldr.js');
+  var TwitterCLDRData = require('./assets/full/' + locale);
+  TwitterCLDR.set_data(TwitterCLDRData);
+  return TwitterCLDR;
+};
