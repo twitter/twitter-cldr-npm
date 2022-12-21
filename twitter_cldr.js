998385f3ae6f0103eb743ae3af8d90c24786d535
lionel-rowe <lionel.rowe@gmail.com>
2022-12-21 18:32:16 +0800

    Add case insensitive matching for locale file names

--- twitter_cldr.js
+++ twitter_cldr.js
@@ -2,8 +2,18 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 
 exports.load = function (locale) {
-  var TwitterCLDR = require('./full/core');
-  var TwitterCLDRDataBundle = require('./full/' + locale);
-  TwitterCLDR.set_data(TwitterCLDRDataBundle);
-  return TwitterCLDR;
+  /**
+   * Generated programmatically via Ruby:
+   *
+   * ```rb
+   * Dir.entries('./full').filter{ |x| x.end_with?('.js') && x != 'core.js' }.map{ |x| x.slice(0, x.length - 3) }.sort
+   * ```
+   */
+  var localeFileNames = ['af', 'ar', 'be', 'bg', 'bn', 'ca', 'cs', 'cy', 'da', 'de', 'de-CH', 'el', 'en', 'en-150', 'en-AU', 'en-CA', 'en-GB', 'en-IE', 'en-SG', 'en-ZA', 'es', 'es-419', 'es-CO', 'es-MX', 'es-US', 'eu', 'fa', 'fi', 'fil', 'fr', 'fr-BE', 'fr-CA', 'fr-CH', 'ga', 'gl', 'he', 'hi', 'hr', 'hu', 'id', 'is', 'it', 'it-CH', 'ja', 'ko', 'lv', 'msa', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'sq', 'sr', 'sv', 'ta', 'th', 'tr', 'uk', 'ur', 'vi', 'zh-cn', 'zh-tw'];
+
+  var localeFileName = localeFileNames.find(function (fileName) {
+    return fileName.toLowerCase() === locale.toLowerCase();
+  });
+
+  return ('./full/' + (localeFileName || locale));
 };
