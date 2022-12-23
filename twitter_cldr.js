001e59b7d91af167d1189e673b448bdfabb7f6dd
lionel-rowe <lionel.rowe@gmail.com>
2022-12-23 09:51:22 +0000

    Update twitter_cldr.js
    Co-authored-by: Cameron Dutro <camertron@gmail.com>

--- twitter_cldr.js
+++ twitter_cldr.js
@@ -6,7 +6,7 @@ exports.load = function (locale) {
    * Generated programmatically via Ruby:
    *
    * ```rb
-   * Dir.entries('./full').filter{ |x| x.end_with?('.js') && x != 'core.js' }.map{ |x| x.slice(0, x.length - 3) }.sort
+   * Dir.entries('./full').select { |x| x.end_with?('.js') && x != 'core.js' }.map { |x| x[0..-4] }.sort
    * ```
    */
   var localeFileNames = ['af', 'ar', 'be', 'bg', 'bn', 'ca', 'cs', 'cy', 'da', 'de', 'de-CH', 'el', 'en', 'en-150', 'en-AU', 'en-CA', 'en-GB', 'en-IE', 'en-SG', 'en-ZA', 'es', 'es-419', 'es-CO', 'es-MX', 'es-US', 'eu', 'fa', 'fi', 'fil', 'fr', 'fr-BE', 'fr-CA', 'fr-CH', 'ga', 'gl', 'he', 'hi', 'hr', 'hu', 'id', 'is', 'it', 'it-CH', 'ja', 'ko', 'lv', 'msa', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'sq', 'sr', 'sv', 'ta', 'th', 'tr', 'uk', 'ur', 'vi', 'zh-cn', 'zh-tw'];
